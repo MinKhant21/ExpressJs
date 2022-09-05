ef711e85dd82854a94aa4ab1759393775dc57ee7
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-09-05 21:04:05 +0630

    StaticAndRouter

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-import express from "express";
-
+import express from 'express';
 const app = express();
+
+import router from './router';
+
+//static file
+app.use(express.static('assests'));
+
+//router file
+app.use(router);
 
 //global middleware
 app.use('/',(req,res,next)=>{
-    console.log('Middleware Fire');
+    console.log('Middleware fired')
     next();
-});
-
-
-app.get('/',(req,res)=>{
-    console.log('hello');
+})
+app.use('/:id',(req,res)=>{
+    console.log(req.params.id);
 })
 
 const port = 3000;
